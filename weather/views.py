76791803a32cfd44534e1c097d68485e4adf52bf
Vik Sir <vsirotkin15@gmail.com>
2024-07-21 08:38:49 +0300

    initial refinement for city data displaying

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,4 @@
+# views.py
 import json
 from django.shortcuts import render
 from .utils import fetch_weather_data
@@ -6,10 +7,14 @@ def home(request):
     with open('weather/cities.json', 'r') as file:
         cities = json.load(file)
     
+    city_names = [city['name'] for city in cities]
     latitudes = [city['latitude'] for city in cities]
     longitudes = [city['longitude'] for city in cities]
-    city_names = [city['name'] for city in cities]
-    
     weather_data = fetch_weather_data(latitudes, longitudes)
     combined_data = list(zip(city_names, weather_data))
-    return render(request, 'weather/home.html', {'combined_data': combined_data})
+
+    selected_city = request.GET.get('city')
+    if selected_city:
+        combined_data = [(name, data) for name, data in combined_data if name == selected_city]
+
+    return render(request, 'weather/home.html', {'combined_data': combined_data, 'city_names': city_names})
