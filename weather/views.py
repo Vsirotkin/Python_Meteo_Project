bee997b656479b510da09555fca8929e580b6b23
Vik Sir <vsirotkin15@gmail.com>
2024-07-21 08:04:35 +0300

    rebuilt fetching cities
    now views takes city names from json file

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,15 @@
-# myapp/views.py
+import json
 from django.shortcuts import render
 from .utils import fetch_weather_data
 
 def home(request):
-    latitudes = [52.52, 56.3287, 40.7143, 52.5244]  # Example latitudes
-    longitudes = [13.41, 44.002, -74.006, 13.4105]  # Example longitudes
-    city_names = ["Moscow", "Nizhny Novgorod", "New York", "Berlin"]
+    with open('weather/cities.json', 'r') as file:
+        cities = json.load(file)
+    
+    latitudes = [city['latitude'] for city in cities]
+    longitudes = [city['longitude'] for city in cities]
+    city_names = [city['name'] for city in cities]
+    
     weather_data = fetch_weather_data(latitudes, longitudes)
     combined_data = list(zip(city_names, weather_data))
     return render(request, 'weather/home.html', {'combined_data': combined_data})
